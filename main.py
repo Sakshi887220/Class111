6d133937b7d2cd98b310ea2bdcdf99968240670e
Sakshi <70653660+Sakshi887220@users.noreply.github.com>
2022-07-13 19:31:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ for i in range(0,1000):
 
 
 ## calculating mean and standard_deviation of the sampling distribution.
-std_deviation = statistics.stdev(mean_list)
+
 # mean = statistics.mean(mean_list)
 # print("mean of sampling distribution:- ",mean)
 # print("Standard deviation of sampling distribution:- ", std_deviation)
@@ -55,9 +55,7 @@ std_deviation = statistics.stdev(mean_list)
 
 
 ## findig the standard deviation starting and ending values
-first_std_deviation_start, first_std_deviation_end = mean-std_deviation, mean+std_deviation
-second_std_deviation_start, second_std_deviation_end = mean-(2*std_deviation), mean+(2*std_deviation)
-third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mean+(3*std_deviation)
+
 # print("std1",first_std_deviation_start, first_std_deviation_end)
 # print("std2",second_std_deviation_start, second_std_deviation_end)
 # print("std3",third_std_deviation_start,third_std_deviation_end)
@@ -102,12 +100,9 @@ third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mea
 
 # finding the mean of the THIRD data (STUDENTS WHO GOT FUNSHEET) and plotting it on the plot.
 df = pd.read_csv("data3.csv")
-data = df["Math_score"].tolist()
-mean_of_sample3 = statistics.mean(data)
-print("mean of sample3:- ",mean_of_sample3)
+
 fig = ff.create_distplot([mean_list], ["student marks"], show_hist=False)
 fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
 fig.add_trace(go.Scatter(x=[mean_of_sample3, mean_of_sample3], y=[0, 0.17], mode="lines", name="MEAN OF STUDNETS WHO GOT FUNSHEETS"))
-fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2 END"))
-fig.add_trace(go.Scatter(x=[third_std_deviation_end, third_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 3 END"))
+
 fig.show()
